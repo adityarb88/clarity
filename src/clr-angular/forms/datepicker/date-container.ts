a5122bd0f7023e1e8114ef25565fe142119e3dec
Aditya Bhandari <adityab@vmware.com>
2018-01-27 15:02:31 -0500

    [NG] Datepicker Initial Commit
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

---
+++ src/clr-angular/forms/datepicker/date-container.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import {Component} from "@angular/core";
+import {IfOpenService} from "../../utils/conditional/if-open.service";
+
+@Component({
+    selector: "clr-date-container",
+    template: `
+        <ng-content></ng-content>
+        <button 
+            type="button" 
+            class="datepicker-trigger" 
+            (click)="toggleCalendar($event)">
+            <clr-icon shape="calendar"></clr-icon>
+        </button>
+        <!--clr-calendar *clrIfOpen clrFocusTrap></clr-calendar-->
+    `,
+    providers: [IfOpenService],
+    host: {"[class.date-container]": "true"}
+})
+export class ClrDateContainer {
+    constructor(public _ifOpenService: IfOpenService) {}
+
+    /**
+     * Toggles the calendar.
+     */
+    toggleCalendar(event: MouseEvent) {
+        this._ifOpenService.toggleWithEvent(event);
+    }
+}
