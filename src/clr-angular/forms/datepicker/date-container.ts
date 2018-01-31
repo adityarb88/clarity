20bf53c2039692275c1a608e931a2737740c9228
Aditya Bhandari <adityab@vmware.com>
2018-01-31 11:54:21 -0500

    [NG] Date IO Service Refactor
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/clr-angular/forms/datepicker/date-container.ts
+++ src/clr-angular/forms/datepicker/date-container.ts
@@ -7,6 +7,9 @@ import {Component} from "@angular/core";
 import {IfOpenService} from "../../utils/conditional/if-open.service";
 import {LocaleHelperService} from "./providers/locale-helper.service";
 import {DateIOService} from "./providers/date-io.service";
+import {DateNavigationService} from "./providers/date-navigation.service";
+import {DayModel} from "./model/day.model";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
     selector: "clr-date-container",
@@ -20,16 +23,42 @@ import {DateIOService} from "./providers/date-io.service";
         </button>
         <clr-datepicker-view-manager *clrIfOpen clrFocusTrap></clr-datepicker-view-manager>
     `,
-    providers: [IfOpenService, LocaleHelperService, DateIOService],
+    providers: [IfOpenService, LocaleHelperService, DateIOService, DateNavigationService],
     host: {"[class.date-container]": "true"}
 })
 export class ClrDateContainer {
-    constructor(public _ifOpenService: IfOpenService) {}
+    private _sub: Subscription;
+
+    constructor(
+        private _ifOpenService: IfOpenService,
+        private _dateNavigationService: DateNavigationService,
+        private _dateIOService: DateIOService) {
+        this._sub = this._ifOpenService.openChange.subscribe((open) => {
+            if (open) {
+                this.initializeCalendar();
+            }
+        });
+    }
 
     /**
      * Toggles the calendar.
      */
     toggleCalendar(event: MouseEvent) {
         this._ifOpenService.toggleWithEvent(event);
+    }
+
+    private initializeCalendar(): void {
+        this.processUserInput();
+        this._dateNavigationService.initializeCalendar();
+    }
+
+    private processUserInput(): void {
+        const date: Date = this._dateIOService.date;
+        if (date) {
+            const dayModel: DayModel = new DayModel(date.getFullYear(), date.getMonth(), date.getDate());
+            this._dateNavigationService.selectedDay = dayModel;
+        } else {
+            this._dateNavigationService.selectedDay = null;
+        }
     }
 }
