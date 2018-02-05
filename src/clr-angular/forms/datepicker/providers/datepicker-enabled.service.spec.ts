bed9857b0df06904855619058e050f62ea570de9
Aditya Bhandari <adityab@vmware.com>
2018-02-05 15:23:57 -0500

    Comment Out Datepicker Enabled Service
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/clr-angular/forms/datepicker/providers/datepicker-enabled.service.spec.ts
+++ src/clr-angular/forms/datepicker/providers/datepicker-enabled.service.spec.ts
@@ -4,39 +4,8 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import {DatepickerEnabledService} from "./datepicker-enabled.service";
-import {DATEPICKER_ENABLE_BREAKPOINT} from "../../../utils/breakpoints/breakpoints";
-
 export default function () {
     xdescribe("Datepicker Enabled Service", () => {
-        let datepickerEnabledService: DatepickerEnabledService;
-        const realUserAgent: string = window.navigator.userAgent;
-        const readInnerWidth: number = window.innerWidth;
-
-        function overrideUserAgent(userAgent: string): void {
-            const getter: string = "__defineGetter__";
-            window.navigator[getter]('userAgent', function(){
-                return userAgent;
-            });
-        }
-
-        function resetUserAgent(): void {
-            const getter: string = "__defineGetter__";
-            window.navigator[getter]('userAgent', function(){
-                return realUserAgent;
-            });
-        }
-
-        beforeEach(() => {
-            datepickerEnabledService = new DatepickerEnabledService();
-        });
-
-        it("isEnabled returns false if breakpoint and userAgent conditions do not match", () => {
-            //expect(datepickerEnabledService.isEnabled).toBe(true);
-            //overrideUserAgent("Mobile");
-        });
-
-        it("", () => {
-        });
     });
 }
+
